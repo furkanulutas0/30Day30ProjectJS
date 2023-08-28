1199570f3824276099b6f2785eb2b398ad456a26
Furkan Ulutaş <furkanulutas@icloud.com>
2023-08-28 18:14:00 +0300

    First Commit

---
+++ DrumKit/index.js
@@ -0,0 +1,16 @@
+﻿window.addEventListener('keydown', function(e) {
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = this.document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!sound) return;
+    
+    key.classList.add('playing');
+    sound.currentTime = 0;
+    sound.play();
+
+});
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', function(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');   
+}));
---
+++ JsClock/script.js
@@ -0,0 +1,24 @@
+﻿const second_arrow = document.querySelector('.second-hand');
+const minute_arrow = document.querySelector('.min-hand');
+const hour_arrow = document.querySelector('.hour-hand');
+const anim = document.querySelector('.hand');
+function setDate() {
+    const now = new Date();
+    const sec = now.getSeconds();
+
+    const secDegree = ((sec/60)*360) + 90; 
+    second_arrow.style.transform = `rotate(${secDegree}deg)`;
+
+    const min = now.getMinutes();
+    const minDegree = ((min/60)*360)+(secDegree/60) +90;
+    minute_arrow.style.transform = `rotate(${minDegree}deg)`;
+
+
+    const hour = now.getHours();
+    const hourDegree = ((hour/12)*360)+(minDegree/360) +90;
+    hour_arrow.style.transform = (`rotate(${hourDegree}deg)`);
+}
+
+setInterval(() => {
+    setDate();
+}, 1000);
