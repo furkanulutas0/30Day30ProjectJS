dc32450aaa8525e8aef96a8dc0e2a8f74355d4a0
Furkan Ulutaş <furkanulutas@icloud.com>
2023-09-03 13:13:57 +0300

    Day 5 - FlexGallery

---
+++ FlexGallery/script.js
@@ -0,0 +1,11 @@
+﻿const cards = document.querySelectorAll(".panel");
+
+cards.forEach(card => card.addEventListener('click',function() {
+    this.classList.toggle('open');
+}));
+
+cards.forEach(openCard => openCard.addEventListener('transitionend',function(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}));
